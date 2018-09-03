29437c75af1ae12153e094c84798a816bbab266f
Zach Elliott <elliottzach86@gmail.com>
2018-09-02 20:15:29 -0400

    Updated REAMDE.

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # dictionary-cli
 
+### Supported Languages   
+* English   
+
 ### Dependencies
 Use pip for Python3 to install the following dependencies:   
-* requests   
+* requests (http://docs.python-requests.org/en/master/)  
+* vocabulary (https://vocabulary.readthedocs.io/en/latest/)
+
+### Usage    
+To define a word:   
+define <word> [options]   
+   
+To find synonyms:
+synonym <word> [options]
+
+Options:
+  --lang <language>
+    - Specify the language of the word entered
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import requests
+from vocabulary.vocabulary import Vocabulary as vb
+
+# Users requests the definition of a word
+def definition_request(word):
+
+    response = vb.meaning(word)
+    print(response)
+
+# User requests synonyms of a word
+def synonym_request(word):
+
+    response = vb.synonym(word)
+    print(response)
+
+
+# Test here
+
+# Valid word
+definition_request('hello')
+synonym_request('hello')
+
+# Invalid word
+definition_request('sdifsdf')
+synonym_request('sdifsdf')
